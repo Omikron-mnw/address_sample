5347b82f698e0d28e4ca19baad6a9fc5ccc4410e
mnwhryk <minomushi.ff15.07.08@gmail.com>
2020-12-12 12:15:23 +0000

    address-v2

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -5,6 +5,6 @@ class UserNotifierMailer < ApplicationMailer
         # @greeting = "Hi"
         @user = user
         @url = "https://7f434ca976cd463ab69e2734f5702628.vfs.cloud9.us-east-1.amazonaws.com"
-        mail( :to => "minomushi.ff15.07.08@gmail.com(実験用の受信元アドレス)", :subject => "会員登録が完了しました。" )
+        mail( :to => @user.email, :subject => "会員登録が完了しました。" )
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
     :address => 'smtp.gmail.com',
     :port => 587,
     :domain => 'gmail.com',
-    :user_name => ENV[""],
-    :password => ENV[""],
+    :user_name => ENV["USER_NAME"],
+    :password => ENV["PASSWARD"],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
